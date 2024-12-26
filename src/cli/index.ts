8b463752db4618ca1aa9dc26a682c927743d4478
dractw <dractw@gmail.com>
2024-12-26 14:36:26 +0300

    [cli] create signed tx

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -48,7 +48,56 @@ program
       .map((fulfilled) => fulfilled?.value)
       .filter(Boolean);
 
+    console.log('OK')
     console.log(keys);
+  });
+
+program
+  .command('tx')
+  .description('Create signed transaction')
+  .option('-p, --phrase <phrase>', 'Generate key-pairs from mnemonic')
+  .option('-w, --wallet <wallet>', 'wallet ticker, e.g. BTC')
+  .option('-a, --amount <amount>', 'Amount, e.g. 1.35')
+  .option('-r, --recepient <recepient>', 'Recepient address')
+  .option('-m, --memo <memo>', 'Memo info')
+  .action(async (options: { wallet: string, amount: string, recepient: string, phrase?: string, memo?: string }): Promise<void> => {
+    console.log('Init mnemonic...');
+    const mnemonic = await initializeMnemonic(options.phrase);
+
+    console.log('OK');
+
+    console.log(`Create ${options.wallet} wallet...`);
+    const wallets = await createWallets({id: options.wallet});
+    const wallet = wallets.find((wallet) => wallet.id.toLowerCase() === options.wallet.toLowerCase());
+
+    if(!wallet) {
+      throw new Error(`Failed to create ${options.wallet}, not supported`);
+    }
+    console.log('OK');
+
+    const validAddress = await wallet.validateAddress(options.recepient) as unknown as boolean;
+
+    if(!validAddress) {
+      throw new Error(`Invalid recepient address: ${options.recepient}`);
+    }
+
+    console.log('Generate key pairs...');
+    await Promise.allSettled(
+      wallets.map(async (wallet) => generateKeys(wallet, mnemonic)),
+    );
+    console.log('OK')
+
+    console.log('Create transaction...')
+    const tx = await wallet.createTransaction({amount: wallet.toMinimalUnit(options.amount), address: options.recepient, memo: options.memo})
+      .catch((err) => {
+        console.log('Failed to create transaction...\n', err.message);
+      });
+
+    if(tx) {
+      console.log(`Signed tx:\n${tx}`)
+    }
+
+
   });
 
 program.parse(process.argv);
