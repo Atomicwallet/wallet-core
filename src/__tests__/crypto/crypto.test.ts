9d907838cb0ad783ad7bd2e9f7243aa3cfbc08e0
dractw <dractw@gmail.com>
2024-12-30 22:52:25 +0300

    [tests/crypto]

---
+++ src/__tests__/crypto/crypto.test.ts
@@ -0,0 +1,5 @@
+import { default as BTCCoinTest } from '@/coins/collection/BTCCoin.test';
+import { default as ETHCoinTest } from '@/coins/collection/ETHCoin.test';
+
+BTCCoinTest();
+ETHCoinTest();
