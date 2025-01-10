9e3ad3083be874eb746fb1b4658e055003ffe4f1
dractw <dractw@gmail.com>
2025-01-10 19:27:23 +0300

    tests

--- src/coins/collection/KMDCoin.test.ts
+++ src/coins/collection/KMDCoin.test.ts
@@ -17,4 +17,15 @@ if (!wallet) {
   throw new Error(`Failed to initialize ${id} wallet`);
 }
 
+jest.spyOn(wallet, 'getFee').mockReturnValue(Promise.resolve(new wallet.BN('45630')));
+
+// @ts-expect-error unimplemented in abstract class
+const originalSign = wallet.signTransaction.bind(wallet);
+// @ts-expect-error mixin overload
+jest.spyOn(wallet, 'signTransaction').mockImplementation((txBuilder, inputs, privateKey) => {
+  txBuilder.tx.locktime = 1736526236;
+
+  return originalSign(txBuilder, inputs, privateKey);
+});
+
 generateWalletTests(wallet);
