81746efa79196dcddef2015a9bdc4a316b4bc76a
dractw <dractw@gmail.com>
2025-01-17 13:51:36 +0300

    ConfigManagerInterface

--- src/abstract/types/index.ts
+++ src/abstract/types/index.ts
@@ -1,3 +1,11 @@
+export type ConfigManagerResponse = Array<any> | Record<string, any>;
+
+export interface ConfigManagerInterface {
+  register(id: string): void;
+  get(id: string): Promise<ConfigManagerResponse>;
+  getLocal(id: string): Promise<ConfigManagerResponse | undefined>;
+}
+
 export * from './abstractWalletTypes';
 export * from './coinTypes';
 export * from './tokenTypes';
