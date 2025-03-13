d054a1096001d13f6b9bdfb56c62e36e686ee256
dractw <dractw@gmail.com>
2025-03-13 19:55:28 +0300

    jest add transform ignore patterns

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,14 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json', babelConfig: true }],
-    '^.+\\.js$': 'babel-jest',
-  },
   moduleNameMapper: {
     '^src/(.*)$': '<rootDir>/src/$1',
     '@/(.*)$': '<rootDir>/src/$1',
   },
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
-  transformIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json', babelConfig: true }],
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!ontology-ts-sdk|nem-sdk)'],
 };
